8f32e52f334354f8b0cb27ee4cb5aae48a35ce4b
Gregg H. Mac Laptop <greggory.png@gmail.com>
2023-06-15 13:48:49 -0700

    Update chartStorage.test.js

--- jest/chartStorage.test.js
+++ jest/chartStorage.test.js
@@ -8,9 +8,16 @@ afterEach(() => {
     window.localStorage.clear()
 })
 
-test("Run loadAllSavedCharts() when no charts are in localStorage", function() {
-    expected_value = []
+test("Save a single chart, then load it with loadAllSavedCharts()", function() {
+    input_value = [
+        {"x": 1, "y": 1, "z": 1}
+    ]
+    expected_value = [[
+        {"x": 1, "y": 1, "z": 1}
+    ]]
+    chartStorage.saveChart(input_value)
     charts = chartStorage.loadAllSavedCharts()
+
     expect(charts).toStrictEqual(expected_value)
 })
 
@@ -26,22 +33,39 @@ test("Save a single chart, then load it with loadSavedChart()", function() {
     expect(charts).toStrictEqual(expected_value)
 })
 
-test("Save a single chart, then load it with loadAllSavedCharts()", function() {
+test("Set current chart data with updateCurrentChartData(), then load it with loadCurrentChartData()", function() {
     input_value = [
         {"x": 1, "y": 1, "z": 1}
     ]
     expected_value = [
-        [
         {"x": 1, "y": 1, "z": 1}
-        ]
     ]
-    chartStorage.saveChart(input_value)
-    charts = chartStorage.loadAllSavedCharts()
+    chartStorage.updateCurrentChartData(input_value)
+    charts = chartStorage.loadCurrentChartData()
 
     expect(charts).toStrictEqual(expected_value)
 })
 
-test("Save a few charts, then load them with loadAllSavedCharts()", function() {
+test("Save a chart with multiple entries, then load it into current chart data", function() {
+    input_value = [
+        {"x": 1, "y": 1, "z": 1},
+        {"x": 2, "y": 2, "z": 2},
+        {"x": 3, "y": 3, "z": 3}
+    ]
+    expected_value = [
+        {"x": 1, "y": 1, "z": 1},
+        {"x": 2, "y": 2, "z": 2},
+        {"x": 3, "y": 3, "z": 3}
+    ]
+    chartStorage.saveChart(input_value)
+    saved_chart = chartStorage.loadSavedChart(0)
+    chartStorage.updateCurrentChartData(saved_chart)
+    charts = chartStorage.loadCurrentChartData()
+
+    expect(charts).toStrictEqual(expected_value)
+})
+
+test("Save a few charts with multiple entries, then load them with loadAllSavedCharts()", function() {
     input1 = [
         {"x": 1, "y": 0, "z": 1},
         {"x": 4, "y": 5, "z": 6}
@@ -74,4 +98,10 @@ test("Save a few charts, then load them with loadAllSavedCharts()", function() {
     chartStorage.saveChart(input2)
     chartStorage.saveChart(input3)
     charts = chartStorage.loadAllSavedCharts()
+})
+
+test("Run loadAllSavedCharts() when no charts are in localStorage", function() {
+    expected_value = []
+    charts = chartStorage.loadAllSavedCharts()
+    expect(charts).toStrictEqual(expected_value)
 })
